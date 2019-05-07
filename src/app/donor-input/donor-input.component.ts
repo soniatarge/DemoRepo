9689c60e7014158b240dc2057a690fad1f155cd7
soniatarge <49725214+soniatarge@users.noreply.github.com>
2019-05-07 12:54:02 -0400

    Add files via upload

---
+++ src/app/donor-input/donor-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donor-input',
+  templateUrl: './donor-input.component.html',
+  styleUrls: ['./donor-input.component.css']
+})
+export class DonorInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
